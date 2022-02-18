d942406334763afa6fc86641f14f271ead4473b9
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-18 14:44:21 +0530

    added room modles

---
+++ server/models/roomModels.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const roomSchema = mongoose.Schema({
+    roomId: { type: String, required: true ,unique:true},
+    roomType:{type:String,required:true},
+    picture: {
+      type: String,
+  
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  });
+  const User = mongoose.model("User", userSchema);
+  
---
+++ server/routes/roomRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+// router.route("/").post(protect, accessChat);
+// router.route("/").get(protect, fetchRooms);
+// router.route("/group").post(protect, bookRoom);
+// router.route("/rename").put(protect, cancelRoomBooking);
+// router.route("/").put(protect, removeFromGroup);
+// router.route("/").put(protect, addToGroup);
+module.exports = router;
