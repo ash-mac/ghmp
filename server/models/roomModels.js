1a2e61c1ee9bc66dc4147d8e92a2a8b3195bc134
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-18 18:10:42 +0530

    added roomModels

--- server/models/roomModels.js
+++ server/models/roomModels.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+
 const roomSchema = mongoose.Schema({
-    roomId: { type: String, required: true ,unique:true},
-    roomType:{type:String,required:true},
-    picture: {
-      type: String,
-  
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    },
-  });
-  const User = mongoose.model("User", userSchema);
-  
+  roomId: { type: String, required: true, unique: true },
+  roomType: { type: String, required: true },
+  isBooked: { type: Boolean, required: true },
+  picture: {
+    type: String,
+
+    default:
+      "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+  },
+});
+const User = mongoose.model("User", userSchema);
