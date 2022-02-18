f4b3a30895aa94fc10993a06a12f783377a8de50
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-18 14:07:42 +0530

    added user models

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.ATLAS_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
