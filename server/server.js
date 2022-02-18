7c50bcb8f79af8eefe2474dff59f3c9a11812e34
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-18 13:28:20 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -8,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());// To accept json data
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true}
