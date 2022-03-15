2745bcb72ea46a437750a535ed22b8b27cb34488
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-03-15 12:56:10 +0530

    added to compare password

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,74 @@
+const expressAsyncHandler = require("express-async-handler");
+const User = require("../Models/userModel");
+const res = require("express/lib/response");
+const generateToken = require("../config/generateToken");
+
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all the fields");
+  }
+    const domain = email.substring(email.length - 12);
+    if (domain !== "iitrpr.ac.in") {
+        res.status(400);
+        throw new Error("Only people belonging to IIT Ropar can register");
+        } else {
+          
+        }
+    //@iitrpr.ac.in
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User alredy Exist");
+  }
+    
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create the user");
+  }
+});
+const authUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+});
+// /api/user?search=apple
+const allUsers = expressAsyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+module.exports = { registerUser, authUser, allUsers };
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  designation: { type: String, required: true },
-  picture: {
-    type: String,
-
-    default:
-      "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+const bcrypt = require("bcryptjs");
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
+  { timestamps: true },
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
 });
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
