4a2f660bfdbcd9a4a7788f00647d990a30edb0ad
Ashish Sharma <2019mcb1213@iitrpr.ac.in>
2022-04-13 12:29:29 +0530

    added contact page

---
+++ client/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+export const Contact = () => {
+    return (
+        <div className="contact">
+            <h2>We are here</h2>
+            <div className="map">
+                <iframe title="guest_house_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3421.064012629328!2d76.47458791421133!3d30.968695781274825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3905554ef0fb26eb%3A0x34208d47d8128b45!2sIIT%20ROPAR%20GUEST%20HOUSE!5e0!3m2!1sen!2sin!4v1649410593192!5m2!1sen!2sin" width="600" height="450" style={{ border: '0' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    )
+}
